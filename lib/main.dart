7c33911ac425efa08e66aeb7a8f930e684cbaded
Luana Dias <luana.dias@colab.re>
2023-05-28 18:53:08 -0300

    criando app versao inicial

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatelessWidget {
+  const Expenses({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: const Center(
+        child: Text('Versao Inicial'),
+      ),
+    );
+  }
+}
