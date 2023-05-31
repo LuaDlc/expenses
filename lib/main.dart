5ee82e2d30587ca40aa7f4fa016581a1e3946c02
Luana Dias <luana.dias@colab.re>
2023-05-30 21:10:38 -0300

    usando column, container e row, alterando estilos

--- lib/main.dart
+++ lib/main.dart
@@ -38,8 +38,8 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const <Widget>[
-          SizedBox(
+        children: <Widget>[
+          const SizedBox(
             child: Card(
               color: Colors.blue,
               child: Text('Grafico'),
@@ -48,9 +48,45 @@ class MyHomePage extends StatelessWidget {
           ),
           Column(
               children: _transactions.map((tr) {
-            return const Card(
-              child: Text(tr.title),
-            );
+            return Card(
+                child: Row(
+              children: <Widget>[
+                Container(
+                  // o container aqui é necessario pois precisa dele pra add estilos
+                  margin: const EdgeInsets.symmetric(
+                      horizontal: 15.0, vertical: 10.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 137, 14, 159),
+                        width: 2.0),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    tr.value.toString(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // titulo de data alinhados em uma coluna
+                  children: <Widget>[
+                    Text(
+                      tr.title!,
+                      style: const TextStyle(
+                          fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                    Text(tr.date.toString(),
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Color.fromARGB(255, 118, 116, 116),
+                        )), //to string pois text recebe tipo string
+                  ],
+                )
+              ],
+            ));
           }).toList())
         ],
       ),
